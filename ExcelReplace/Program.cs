9423b5a6e42ba41614b59c517b6655c7bb7a4723
chisty sohel <chistysohail@gmail.com>
2023-11-17 00:47:20 -0500

    updated for case sensitivity

--- ExcelReplace/Program.cs
+++ ExcelReplace/Program.cs
@@ -28,11 +28,12 @@ namespace ExcelReplace
 
                         Console.WriteLine($"Looking for: {findValue}, to replace with: {replaceValue}");
 
-                        // Check if the word exists in the file
-                        if (fileContent.Contains(findValue))
+                        // Check if the word exists in the file (case-insensitive)
+                        if (Regex.IsMatch(fileContent, Regex.Escape(findValue), RegexOptions.IgnoreCase))
                         {
                             Console.WriteLine($"Found '{findValue}' in the file. Replacing...");
-                            fileContent = Regex.Replace(fileContent, Regex.Escape(findValue), replaceValue);
+                            // Replace in the TypeScript file content (case-insensitive)
+                            fileContent = Regex.Replace(fileContent, Regex.Escape(findValue), replaceValue, RegexOptions.IgnoreCase);
                         }
                         else
                         {
@@ -52,6 +53,62 @@ namespace ExcelReplace
         }
     }
 }
+
+
+//using ClosedXML.Excel;
+//using System;
+//using System.IO;
+//using System.Text.RegularExpressions;
+
+//namespace ExcelReplace
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            try
+//            {
+//                // Load the Excel workbook
+//                using (var workbook = new XLWorkbook("path/to/your/excelfile.xlsx"))
+//                {
+//                    var worksheet = workbook.Worksheet(1); // assuming data is in the first worksheet
+
+//                    // Read the TypeScript file
+//                    string tsFilePath = "path/to/your/app.const.ts";
+//                    string fileContent = File.ReadAllText(tsFilePath);
+
+//                    // Iterate through the rows of the Excel file
+//                    foreach (var row in worksheet.RangeUsed().Rows())
+//                    {
+//                        string findValue = row.Cell(1).GetValue<string>();    // Column A
+//                        string replaceValue = row.Cell(2).GetValue<string>(); // Column B
+
+//                        Console.WriteLine($"Looking for: {findValue}, to replace with: {replaceValue}");
+
+//                        // Check if the word exists in the file
+//                        if (fileContent.Contains(findValue))
+//                        {
+//                            Console.WriteLine($"Found '{findValue}' in the file. Replacing...");
+//                            fileContent = Regex.Replace(fileContent, Regex.Escape(findValue), replaceValue);
+//                        }
+//                        else
+//                        {
+//                            Console.WriteLine($"'{findValue}' not found in the file.");
+//                        }
+//                    }
+
+//                    // Save the modified TypeScript file
+//                    File.WriteAllText(tsFilePath, fileContent);
+//                    Console.WriteLine("File saved successfully.");
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine($"An error occurred: {ex.Message}");
+//            }
+//        }
+//    }
+//}
 //using ClosedXML.Excel;
 //using System;
 //using System.Text.RegularExpressions;
