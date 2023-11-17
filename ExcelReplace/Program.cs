56a3e8ea1874d996f6f60e73d16180d73699cda0
chisty sohel <chistysohail@gmail.com>
2023-11-17 00:24:47 -0500

    updated v2

--- ExcelReplace/Program.cs
+++ ExcelReplace/Program.cs
@@ -9,27 +9,45 @@ namespace ExcelReplace
     {
         static void Main(string[] args)
         {
-            // Load the Excel workbook
-            using (var workbook = new XLWorkbook("path/to/your/excelfile.xlsx"))
+            try
             {
-                var worksheet = workbook.Worksheet(1); // assuming data is in the first worksheet
-
-                // Read the TypeScript file
-                string tsFilePath = "path/to/your/app.const.ts";
-                string fileContent = File.ReadAllText(tsFilePath);
-
-                // Iterate through the rows of the Excel file
-                foreach (var row in worksheet.RangeUsed().Rows())
+                // Load the Excel workbook
+                using (var workbook = new XLWorkbook("path/to/your/excelfile.xlsx"))
                 {
-                    string findValue = row.Cell(1).GetValue<string>();    // Column A
-                    string replaceValue = row.Cell(2).GetValue<string>(); // Column B
+                    var worksheet = workbook.Worksheet(1); // assuming data is in the first worksheet
 
-                    // Replace in the TypeScript file content
-                    fileContent = Regex.Replace(fileContent, findValue, replaceValue);
+                    // Read the TypeScript file
+                    string tsFilePath = "path/to/your/app.const.ts";
+                    string fileContent = File.ReadAllText(tsFilePath);
+
+                    // Iterate through the rows of the Excel file
+                    foreach (var row in worksheet.RangeUsed().Rows())
+                    {
+                        string findValue = row.Cell(1).GetValue<string>();    // Column A
+                        string replaceValue = row.Cell(2).GetValue<string>(); // Column B
+
+                        Console.WriteLine($"Looking for: {findValue}, to replace with: {replaceValue}");
+
+                        // Check if the word exists in the file
+                        if (fileContent.Contains(findValue))
+                        {
+                            Console.WriteLine($"Found '{findValue}' in the file. Replacing...");
+                            fileContent = Regex.Replace(fileContent, Regex.Escape(findValue), replaceValue);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"'{findValue}' not found in the file.");
+                        }
+                    }
+
+                    // Save the modified TypeScript file
+                    File.WriteAllText(tsFilePath, fileContent);
+                    Console.WriteLine("File saved successfully.");
                 }
-
-                // Save the modified TypeScript file
-                File.WriteAllText(tsFilePath, fileContent);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
             }
         }
     }
